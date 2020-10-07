6bbef3c7deaa39085978aec50cd33bee4e18285d
matthiasyychan <hawkeagle@runbox.com>
2020-10-07 15:08:53 +0800

    Adding Decorators

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,17 +1,11 @@
-// home.dart
 import 'package:flutter/material.dart';
 
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
+        title: Text('Images'),
       ),
       body: SafeArea(
         child: SingleChildScrollView(
@@ -20,6 +14,10 @@ class _HomeState extends State<Home> {
             child: Column(
               children: <Widget>[
                 const ImagesAndIconWidget(),
+                Divider(),
+                const BoxDecoratorWidget(),
+                Divider(),
+                const InputDecoratorsWidget(),
               ],
             ),
           ),
@@ -33,6 +31,7 @@ class ImagesAndIconWidget extends StatelessWidget {
   const ImagesAndIconWidget({
     Key key,
   }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -40,7 +39,7 @@ class ImagesAndIconWidget extends StatelessWidget {
       children: <Widget>[
         Image(
           image: AssetImage("assets/images/logo.png"),
-//color: Colors.orange,
+          //color: Colors.orange,
           fit: BoxFit.cover,
           width: MediaQuery.of(context).size.width / 3,
         ),
@@ -52,6 +51,69 @@ class ImagesAndIconWidget extends StatelessWidget {
           Icons.brush,
           color: Colors.lightBlue,
           size: 48.0,
+        ),
+      ],
+    );
+  }
+}
+
+class BoxDecoratorWidget extends StatelessWidget {
+  const BoxDecoratorWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Container(
+        height: 100.0,
+        width: 100.0,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(20.0)),
+          color: Colors.orange,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey,
+              blurRadius: 10.0,
+              offset: Offset(0.0, 10.0),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class InputDecoratorsWidget extends StatelessWidget {
+  const InputDecoratorsWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        TextField(
+          keyboardType: TextInputType.text,
+          style: TextStyle(
+            color: Colors.grey.shade800,
+            fontSize: 16.0,
+          ),
+          decoration: InputDecoration(
+            labelText: "Notes",
+            labelStyle: TextStyle(color: Colors.purple),
+            // border: UnderlineInputBorder(),
+            // enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.lightGreen)),
+            border: OutlineInputBorder(),
+          ),
+        ),
+        Divider(
+          color: Colors.lightGreen,
+          height: 50.0,
+        ),
+        TextFormField(
+          decoration: InputDecoration(labelText: 'Enter your notes'),
         ),
       ],
     );
